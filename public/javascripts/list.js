28ff7a702993534fa89bb4241a7b8dbd5e618ea3
Tom Sampic <tom.sampic@insa-lyon.fr>
2019-11-01 12:28:07 +0100

    add btn delete behavior

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,46 +1,57 @@
 let btnsSuccess = document.getElementsByClassName("btn-success");
-let btnsDel = document.getElementsByClassName("btn-danger");
+let btnDel = document.getElementsByClassName("btn-danger")[0];
+
+let checkedButtons = 0;
 
 for (btnSuccess of btnsSuccess) {
   btnSuccess.onclick = successClick;
 }
 
-for (btnDel of btnsDel) {
-  btnDel.onclick = delClick;
-}
+btnDel.onclick = delClick;
 
 function successClick(event) {
   
   // pass color to green
 
   let btn = event.target;
-  
-  console.log(btn.id.substring(0, btn.id.length - 8));
-  console.log(btn.id);
 
   let div = document.getElementById(btn.id.substring(0, btn.id.length - 8));
 
   if (div.className.includes('alert-success')) {
-    div.setAttribute("class", "alert alert-dark row");
+    //cas ou on passe en mode unchecked
+
+    div.setAttribute("class", div.className.replace("alert-success", "alert-dark"));
     btn.innerHTML = 'Check';
-    btn.setAttribute("class", "btn btn-success mr-3 mr-sm-0");
+    btn.setAttribute("class", btn.className.replace("btn-warning", "btn-success"));
+    checkedButtons--;
+    //si il ne reste plus de boutons checked on l'enleve
+    if (checkedButtons == 0) {
+      btnDel.setAttribute("class", btnDel.className.replace("btn-block", "d-none"));
+    }
+
   } else {
-    div.setAttribute("class", "alert alert-success row");
+    //cas ou on passe en mode checked
+
+    div.setAttribute("class", div.className.replace("alert-dark", "alert-success"));
     btn.innerHTML = 'Uncheck';
-    btn.setAttribute("class", "btn btn-warning mr-3 mr-sm-0");
+    btn.setAttribute("class", btn.className.replace("btn-success", "btn-warning"));
+    checkedButtons++;
+
+    btnDel.setAttribute("class", btnDel.className.replace("d-none", "btn-block"));
+
   }
-  console.log("updated");
 
 }
 
-function delClick(btn) {
+function checkDeleteMode() {
+
+}
+
+function delClick(event) {
+
+  let btn = event.target;
   
   // delete from database
-  let div = document.getElementById(btn.id.substring(0, btn.id.length - 4));
-  
-  console.log(btn.id.substring(0, btn.id.length - 4));
   console.log(btn.id);
-
-  
 
 }
