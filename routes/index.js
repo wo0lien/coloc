2c11405720910082f2492c8fad776e5e62887d92
Tom Sampic <tom.sampic@insa-lyon.fr>
2019-11-02 15:37:28 +0100

    responsive home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Ingredient = require('../app/models/ingredient');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'coucou papa' });
+
+  Ingredient.find({}, (err,results)=>{
+    res.render('index', { 'nbItems': String(results.length) });
+  })
+  
 });
 
 module.exports = router;
