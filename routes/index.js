74e3ec6473c4fce9d2ac00104d59e14ab15be303
Tom Sampic <tom.sampic@insa-lyon.fr>
2019-11-06 08:49:04 +0100

    finish deleting list

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Ingredient = require('../app/models/ingredient');
+var Product = require('../API/models/product.model');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Ingredient.find({}, (err,results)=>{
+  Product.find({}, (err, results)=>{
     res.render('index', { 'nbItems': String(results.length) });
   })
   
