16c7e40e5968310f18ca59ecbe75b7c4b2a2df01
Tom Sampic <tom.sampic@insa-lyon.fr>
2019-11-08 12:00:05 +0100

    starting to separate the  login vue component

---
+++ vue/components/login.vue
@@ -0,0 +1,13 @@
+<template lang="jade">
+  button#btnLogin.btn.btn-outline-secondary.text-light.my-2.my-sm-0(data-toggle="modal" data-target="#loginModal") Login
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
